1104f4c29dc609d2f7b93a6e8f9f1cac731d9c85
Banoth-Abhishek <88137044+Banoth-Abhishek@users.noreply.github.com>
2022-10-12 12:10:58 +0530

    Changes have been committed

---
+++ 2.1.functions_and_prototypes.js
@@ -0,0 +1,23 @@
+function Employee(name,designation,yearOfBirth){
+    this.name = name,
+    this.designation = designation,
+    this.yearOfBirth = yearOfBirth
+}
+
+Employee.prototype.calculateAge = function(){
+    console.log('The current age is: '+(2022-this.yearOfBirth));
+}
+console.log(Employee.prototype);
+
+
+let Empl1 = new Employee('Alex','Junior Tester',1995);
+console.log(Empl1);
+Empl1.calculateAge();
+
+let Emp2= new Employee('Dexter', 'Senior Software Developer', 1990); 
+console.log(Emp2) 
+Emp2.calculateAge();
+
+let Emp3= new Employee('Annie', 'Junior HR', 1998); 
+console.log(Emp3) 
+Emp3.calculateAge();
